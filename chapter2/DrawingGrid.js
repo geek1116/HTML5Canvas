11b4b3dc7f31dd587f724056758dd11e6b2de662
geek1116 <1035214549@qq.com>
2020-11-19 17:48:38 +0800

    upload DrawingGrid

---
+++ chapter2/DrawingGrid.js
@@ -0,0 +1,22 @@
+let context = document.querySelector('canvas').getContext('2d');
+
+function drawGrid(color, stepx, stepy) {
+    context.strokeStyle = color;
+    context.lineWidth = 0.5;
+
+    for(let i = stepx + 0.5; i < context.canvas.width; i += stepx) {
+        context.beginPath();
+        context.moveTo(i, 0);
+        context.lineTo(i, context.canvas.height);
+        context.stroke();
+    }
+
+    for(let i = stepy + 0.5; i < context.canvas.height; i += stepy) {
+        context.beginPath();
+        context.moveTo(0, i);
+        context.lineTo(context.canvas.width, i);
+        context.stroke();
+    }
+}
+
+drawGrid('lightgray', 10, 10);
