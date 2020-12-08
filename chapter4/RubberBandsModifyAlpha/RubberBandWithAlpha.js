ac48593446600af757262740ac482dc68d0d104c
geek1116 <1035214549@qq.com>
2020-12-08 21:15:36 +0800

    upload RubberBandWithAlpha

--- chapter4/RubberBandsModifyAlpha/RubberBandWithAlpha.js
+++ chapter4/RubberBandsModifyAlpha/RubberBandWithAlpha.js
@@ -23,7 +23,7 @@ function copyCanvasPixels() {
 }
 
 function captureCanvasPixels() {
-    imageData = context.getImageData(0, 0, canvas.widht, canvas.height);
+    imageData = context.getImageData(0, 0, canvas.width, canvas.height);
     copyCanvasPixels();
 }
 
@@ -39,8 +39,8 @@ function rubberbandStart(x, y) {
 function resotreRubberbandPixels() {
     let deviceWidthOverCSSPixels = imageData.width / canvas.width, deviceHeightOverCSSPixels = imageData.height / canvas.height;
 
-    context.putImage(imageData, 0, 0);
-    context.putImage(imageDataCopy, 0, 0,
+    context.putImageData(imageData, 0, 0);
+    context.putImageData(imageDataCopy, 0, 0,
                         rubberbandRectangle.left + context.lineWidth,
                         rubberbandRectangle.top + context.lineWidth,
                         (rubberbandRectangle.width - 2 * context.lineWidth) * deviceWidthOverCSSPixels,
@@ -48,11 +48,11 @@ function resotreRubberbandPixels() {
 }
 
 function setRubberbandRectangle(x, y) {
-    rubberbandRectangle.left = loc.x < mousedown.x ? loc.x : mousedown.x;
-    rubberbandRectangle.top = loc.y < mousedown.y ? loc.y : mousedown.y;
+    rubberbandRectangle.left = x < mousedown.x ? x : mousedown.x;
+    rubberbandRectangle.top = y < mousedown.y ? y : mousedown.y;
 
-    rubberbandRectangle.width = Math.abs(loc.x - mousedown.x);
-    rubberbandRectangle.height = Math.abs(loc.y - mousedown.y);
+    rubberbandRectangle.width = Math.abs(x - mousedown.x);
+    rubberbandRectangle.height = Math.abs(y - mousedown.y);
 }
 
 function drawRubberband() {
@@ -75,6 +75,23 @@ function rubberbandStretch(x, y) {
     }
 }
 
+function rubberbandEnd() {
+    if(rubberbandRectangle.width > 2 * context.lineWidth &&
+        rubberbandRectangle.height > 2 * context.lineWidth) {
+            
+            context.putImageData(imageData, 0, 0);
+
+            context.drawImage(canvas, rubberbandRectangle.left + context.lineWidth,
+                        rubberbandRectangle.top + context.lineWidth,
+                        rubberbandRectangle.width - 2 * context.lineWidth,
+                        rubberbandRectangle.height - 2 * context.lineWidth,
+                        0, 0, canvas.width, canvas.height);
+    }
+
+    dragging = false;
+    imageData = undefined;
+}
+
 // ------------------------- Event Handlers ---------------------------
 canvas.addEventListener('mousedown', (e) => {
     let loc = windowToCanvas(e.clientX, e.clientY);
@@ -82,9 +99,26 @@ canvas.addEventListener('mousedown', (e) => {
     rubberbandStart(loc.x, loc.y);
 })
 
-canvas.addEventListener('mouseup', (e) => {
+canvas.addEventListener('mousemove', (e) => {
     if(dragging) {
         let loc = windowToCanvas(e.clientX, e.clientY);
         rubberbandStretch(loc.x, loc.y);
     }
 })
+
+canvas.addEventListener('mouseup', (e) => {
+    rubberbandEnd();
+})
+
+image.src = 'NewYork.jpg';
+image.onload = () => {
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+}
+
+resetBtn.addEventListener('click', () => {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(image, 0, 0, canvas.width, canvas.height);
+})
+
+context.strokeStyle = 'navy';
+context.lineWidth = 1.0;
